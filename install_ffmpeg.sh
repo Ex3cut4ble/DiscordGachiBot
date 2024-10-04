e42b96f5d96a9b5aafc8c5a13fa1d70abafa0120
NO-1 <alexandr99998@gmail.com>
2024-10-05 01:14:06 +0300

    sh format

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 curl -sL https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz | tar -Jxf - -C ./
 mv ./ffmpeg-master-latest-linux64-gpl/bin/ffmpeg ./
