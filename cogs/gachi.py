5887f6c377e421a0fddbd4cdffcf00f844d49f13
NO-1 <alexandr99998@gmail.com>
2025-08-27 21:23:13 +0300

    refactor

--- cogs/gachi.py
+++ cogs/gachi.py
@@ -1,3 +1,4 @@
+from disnake import VoiceClient
 from disnake.ext import commands
 from bot.botgui import *
 from bot.musicplayer import *
@@ -12,24 +13,14 @@ class Gachi(commands.Cog):
 
     @commands.slash_command(name='gachi', description='Включает гачи-радио в голосовом канале')
     async def _gachi_cmd(self, interaction: disnake.ApplicationCommandInteraction):
-        voice_client = interaction.guild.voice_client
-
-        if not interaction.user.voice:
-            await interaction.response.send_message(embed=build_error_embed("Вы не подключены к голосовому каналу"),
-                                                    ephemeral=True)
-            return
-
-        if voice_client is not None:
-            if voice_client.is_playing():
-                await interaction.response.send_message(embed=build_error_embed("Бот уже играет гачи-радио"),
-                                                        ephemeral=True)
-                return
+        if not interaction.guild.voice_client:
+            if interaction.author.voice:
+                await interaction.author.voice.channel.connect()
             else:
-                await voice_client.disconnect()
-                voice_client = None
-
-        if voice_client is None:
-            await interaction.user.voice.channel.connect()
+                await interaction.response.send_message(embed=build_error_embed("Вы не подключены к голосовому каналу"), ephemeral=True)
+                return
+        else:
+            await interaction.response.send_message(embed=build_error_embed("Бот уже играет гачи-радио"), ephemeral=True)
 
         try:
             await self._play_gachi(interaction)
